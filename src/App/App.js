2a2edbe39587ad04026bf3d69fef53d07a20f3f4
Stowarzyszenie <maciej.gerka@gmail.com>
2016-12-13 22:04:17 +0100

    dodanie CommonSection z początkową wersją menu

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { CommonSection } from './CommonSection'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CommonSection />
+      </div>
+    );
+  }
+}
+
+export default App;
