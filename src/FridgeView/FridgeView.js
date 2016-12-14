2155455ecbdd256af15d1d552e289ac5e18f1589
Stowarzyszenie <maciej.gerka@gmail.com>
2016-12-14 20:33:27 +0100

    wzór na obiekt "ingredient" i początek prac nad "FridgeView"

--- src/FridgeView/FridgeView.js
+++ src/FridgeView/FridgeView.js
@@ -1,7 +1,42 @@
 import React from 'react'
 
+import { FormGroup, Checkbox } from 'react-bootstrap'
+
 export default () => (
   <div>
     <h1>FridgeView</h1>
+      <FormGroup>
+          <Checkbox inline>
+              1. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              2. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              3. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              4. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              5. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              6. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              7. link do 'ingredients.id'
+          </Checkbox>
+
+          <Checkbox inline>
+              8. link do 'ingredients.id'
+          </Checkbox>
+      </FormGroup>
   </div>
 )
