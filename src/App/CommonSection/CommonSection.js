25685ce13302d8b3239ac6eb794adfbeb5fd7cf7
Maciej Gerka <maciej.gerka@gmail.com>
2016-12-14 15:35:39 +0100

    linki z lewej na menu

--- src/App/CommonSection/CommonSection.js
+++ src/App/CommonSection/CommonSection.js
@@ -9,17 +9,19 @@ export default () => {
                     <a href="#">INSIDE MY FRIDGE</a>
                 </Navbar.Brand>
             </Navbar.Header>
-            <Nav>
-                <NavItem eventKey={1} href="#">Link</NavItem>
+            <Nav pullRight>
+                <NavItem eventKey={1} href="#">Przepisy</NavItem>
                 <NavItem eventKey={2} href="#">Link</NavItem>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
+                {/*<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">*/}
+                    {/*<MenuItem eventKey={3.1}>Action</MenuItem>*/}
+                    {/*<MenuItem eventKey={3.2}>Another action</MenuItem>*/}
+                    {/*<MenuItem eventKey={3.3}>Something else here</MenuItem>*/}
+                    {/*<MenuItem divider />*/}
+                    {/*<MenuItem eventKey={3.3}>Separated link</MenuItem>*/}
+                {/*</NavDropdown>*/}
             </Nav>
         </Navbar>
     )
 }
+
+
