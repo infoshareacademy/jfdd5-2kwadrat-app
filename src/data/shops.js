5f56cfef4ddbd66679fe4cf736a5387c248d5987
Dorota <dorleniec@gmail.com>
2016-12-14 20:11:25 +0100

    stworzenie shops lists
    usunięcie Component z importu bo nigdzie go nie używamy

--- src/data/shops.js
+++ src/data/shops.js
@@ -1,3 +1,114 @@
-/**
- * Created by mg on 14.12.16.
- */
+export default [
+    {
+        id: 1,
+        name: 'biedronka',
+        location: 'GPS-1',
+        logo: 'http://www.jeronimomartins.pt/media/330161/logo_biedronka.jpg',
+        ingriedients: [
+            {
+                ingriedientId: 1,
+                ingriedientPrice: 2.04
+            },
+            {
+                ingriedientId: 2,
+                ingriedientPrice: 4.84
+            },
+            {
+                ingriedientId: 3,
+                ingriedientPrice: 3.04
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: 'lidl',
+        location: 'GPS-2',
+        logo: 'http://www.kreno-consulting.fr/wp-content/uploads/2014/03/Logo_LIDL.png',
+        ingriedients: [
+            {
+                ingriedientId: 4,
+                ingriedientPrice: 10.50
+            },
+            {
+                ingriedientId: 1,
+                ingriedientPrice: 2.15
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'tesco',
+        location: 'GPS-3',
+        logo: 'http://logok.org/wp-content/uploads/2014/12/Tesco-Logo-880x654.png',
+        ingriedients: [
+            {
+                ingriedientId: 6,
+                ingriedientPrice: 7.89
+            },
+            {
+                ingriedientId: 7,
+                ingriedientPrice: 2.24
+            },
+            {
+                ingriedientId: 8,
+                ingriedientPrice: 5.54
+            },
+            {
+                ingriedientId: 4,
+                ingriedientPrice: 3.34
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'stokrotka',
+        location: 'GPS-4',
+        logo: 'http://www.fresh-market.pl/files/570854499/stokrotka1.png',
+        ingriedients: [
+            {
+                ingriedientId: 1,
+                ingriedientPrice: 2.04
+            },
+            {
+                ingriedientId: 5,
+                ingriedientPrice: 4.84
+            },
+            {
+                ingriedientId: 11,
+                ingriedientPrice: 3.04
+            },
+            {
+                ingriedientId: 12,
+                ingriedientPrice: 2.04
+            },
+            {
+                ingriedientId: 3,
+                ingriedientPrice: 4.84
+            },
+            {
+                ingriedientId: 15,
+                ingriedientPrice: 3.04
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'polo-market',
+        location: 'GPS-5',
+        logo: 'http://rabatuj.pl/r/i/r/images/polomarket_logo.png',
+        ingriedients: [
+            {
+                ingriedientId: 10,
+                ingriedientPrice: 2.04
+            },
+            {
+                ingriedientId: 14,
+                ingriedientPrice: 4.84
+            },
+            {
+                ingriedientId: 16,
+                ingriedientPrice: 3.04
+            },
+        ]
+    }
+]
