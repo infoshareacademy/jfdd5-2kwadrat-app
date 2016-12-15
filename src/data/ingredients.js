aca6e2db01cf25af0f6d7d7d902df5ad9f15b633
Maciej Gerka <maciej.gerka@gmail.com>
2016-12-15 15:10:57 +0100

    dodanie zdjęcia z marchewką

--- src/data/ingredients.js
+++ src/data/ingredients.js
@@ -1,82 +1,37 @@
 export default [
-    {
-        id: 1,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 2,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 3,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 4,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 5,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 6,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 7,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 8,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 9,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 10,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 11,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 12,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 13,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 14,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 15,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
-    {
-        id: 16,
-        name: 'marchewka',
-        unitOfMeasure: 'pcs.'
-    },
+  {
+    id: 1,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 2,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 3,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 4,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 5,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
 ]
