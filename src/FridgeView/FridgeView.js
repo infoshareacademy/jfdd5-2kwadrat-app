276c158637350f858b62a01e635c9e62e69de403
Maciej Gerka <maciej.gerka@gmail.com>
2016-12-15 16:19:48 +0100

    FridgeView development

--- src/FridgeView/FridgeView.js
+++ src/FridgeView/FridgeView.js
@@ -11,7 +11,7 @@ export default () => (
       ingredients.map(
         ingredient => {
           return (
-            <Col xs={12} sm={6} md={4} lg={3}>
+            <Col key={ingredient.id} className="ingredientField" xs={12} sm={6} md={4} lg={3}>
               <Image src={ingredient.img} height="50px"/>
               <Checkbox inline>
                 {ingredient.name}
--- src/data/ingredients.js
+++ src/data/ingredients.js
@@ -48,4 +48,11 @@ export default [
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
+  {
+    id: 8,
+    name: 'marchewka',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
 ]
