9d68ac48b18cfbdf96411411d17fcb9e64a45513
Kuba Melson <kubamelson@gmail.com>
2016-12-15 17:00:33 +0100

    Page with just one recipe view indludes description

--- src/RecipeView/RecipeView.js
+++ src/RecipeView/RecipeView.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {Image, Col} from 'react-bootstrap'
+import {recipes} from '../data'
 
-export default (props) => (
-  <div>
-    <h1>RecipeView {props.params.recipeId}</h1>
-  </div>
-)
+
+
+
+export default (props) => {
+  const recipeWithId = recipes.find(
+    recipe => recipe.id === parseInt(props.params.recipeId, 10)
+  )
+  return (
+    <div key = {recipeWithId.id}>
+      <Col xs={12}>
+        <h1>{recipeWithId.name}</h1>
+        <Image src={recipeWithId.image}/>
+        <p>{recipeWithId.description}</p>
+      </Col>
+    </div>
+  )
+}
