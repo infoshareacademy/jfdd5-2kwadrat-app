03adf8987986779e28e57d012baf31c16152475e
Maciej Gerka <maciej.gerka@gmail.com>
2016-12-16 16:24:41 +0100

    FridgeView: checkbox and submit button fully work

--- src/FridgeView/FridgeView.js
+++ src/FridgeView/FridgeView.js
@@ -2,11 +2,8 @@ import React from 'react'
 import './styles.css'
 import {ingredients} from '../data'
 
-import {Checkbox, Image, Col, Row} from 'react-bootstrap'
+import {Image, Col, Row} from 'react-bootstrap'
 
-
-//użyć handleSubmit; nasłuchiwać na kliknięcie checkboxa i jego odkliknięcie
-//dlaczego wyskakuje w konsoli błąd o braku unikalnego ID, a mam go w setState?
 
 export default class FridgeView extends React.Component {
   constructor() {
@@ -16,8 +13,8 @@ export default class FridgeView extends React.Component {
       event.preventDefault()
       this.setState({
         ingredients: this.state.ingredients.concat({
-          id: new Date().getTime(),
-          name: this.state.name
+          name: this.state.ingredients.name,
+          id: this.state.ingredients.id
         })
       }, function () {
         localStorage.setItem('my-app-state', JSON.stringify(this.state))
@@ -29,11 +26,7 @@ export default class FridgeView extends React.Component {
       this.state = JSON.parse(data)
     } else {
       this.state = {
-        ingredients: [
-          {
-            name: ''
-          }
-        ]
+        ingredients: []
       }
     }
   }
@@ -41,27 +34,52 @@ export default class FridgeView extends React.Component {
   render() {
     return (
       <div>
-        <Row>
-          <h1>FridgeView</h1>
-          {ingredients.map(
-            ingredient => {
-              return (
-                <Col key={ingredient.id} xs={12} sm={6} md={4} lg={3}>
-                  <div className="ingredientFieldContent">
-                    <Image src={ingredient.img} height="50px"/>
-                    <Checkbox inline>
+        <form onSubmit={this.handleSubmit}>
+          <Row>
+            <h1>FridgeView</h1>
+            {ingredients.map(
+              ingredient => {
+                return (
+                  <Col key={ingredient.id} xs={12} sm={6} md={4} lg={3}>
+                    <div className="ingredientFieldContent">
+                      <Image src={ingredient.img} height="50px"/>
                       {ingredient.name}
-                    </Checkbox>
-                  </div>
-                </Col>
-              )
-            }
-          )}
-        </Row>
+                      <input
+                        key={ingredient.id}
+                        type="checkbox"
+                        onChange={
+                          event => {
+                            if (event.target.checked === true) {
+                              this.setState(
+                                {
+                                  ingredients: this.state.ingredients.concat(
+                                    {
+                                      name: ingredient.name,
+                                      id: ingredient.id
+                                    })
+                                })
+                            } else {
+                              this.setState(
+                                {
+                                  ingredients: this.state.ingredients.filter(
+                                    item => item.id !== ingredient.id)
+                                }
+                              )
+                            }
+                          }
+                        }
+                      />
+                    </div>
+                  </Col>
+                )
+              }
+            )}
+          </Row>
 
-        <Row>
-          <button type="submit">Wyszukaj przepisy</button>
-        </Row>
+          <Row>
+            <button type="submit">Wyszukaj przepisy</button>
+          </Row>
+        </form>
 
         <Row>
           <h2>Chosen ingredients</h2>
--- src/data/ingredients.js
+++ src/data/ingredients.js
@@ -1,63 +1,154 @@
 export default [
   {
     id: 1,
-    name: 'marchewka',
+    name: 'marchewka1',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 2,
-    name: 'marchewka',
+    name: 'marchewka2',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 3,
-    name: 'marchewka',
+    name: 'marchewka3',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 4,
-    name: 'marchewka',
+    name: 'marchewka4',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 5,
-    name: 'marchewka',
+    name: 'marchewka5',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 6,
-    name: 'marchewka',
+    name: 'marchewka6',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 7,
-    name: 'marchewka',
+    name: 'marchewka7',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 8,
-    name: 'marchewka',
+    name: 'marchewka8',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
   },
   {
     id: 9,
-    name: 'marchewka',
+    name: 'marchewka9',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 10,
+    name: 'marchewka10',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 11,
+    name: 'marchewka11',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 12,
+    name: 'marchewka12',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 13,
+    name: 'marchewka13',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 14,
+    name: 'marchewka14',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 15,
+    name: 'marchewka15',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 16,
+    name: 'marchewka16',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 17,
+    name: 'marchewka17',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 18,
+    name: 'marchewka18',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 19,
+    name: 'marchewka19',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 20,
+    name: 'marchewka20',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 21,
+    name: 'marchewka21',
+    unitOfMeasure: 'pcs.',
+    amount: 0,
+    img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
+  },
+  {
+    id: 22,
+    name: 'marchewka22',
     unitOfMeasure: 'pcs.',
     amount: 0,
     img: 'https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/carrot-128.png'
