2bb1f53e6b4ad0d490b78fd6f62fff7305b1c28c
JakubMelson <kubamelson@gmail.com>
2016-12-16 22:52:57 +0100

    Fighting with inGREDdients names, corrects with shops.inGREDients.find()

--- src/IngredientView/IngredientView.js
+++ src/IngredientView/IngredientView.js
@@ -5,26 +5,26 @@ import {Image, Col} from 'react-bootstrap'
 
 
 export default (props) => {
-  const shopsWithId = shops.find(
-      shop => shop.id === parseInt(props.params.shopId, 10)
+  const ingredientsWithId = ingredients.find(
+      ingredients => ingredients.id === parseInt(props.params.ingredientsId, 10)
   )
   return (
       <div>
         <Col xs={6}>
 
-          <div key={shopsWithId.id}>
+          <div key={ingredientsWithId.id}>
             <Col xs={12}>
-              <h1>{shopsWithId.name}</h1>
-              <Image src={shopsWithId.img}/>
+              <h1>{ingredientsWithId.name}</h1>
+              <Image src={ingredientsWithId.img}/>
               <div>
                 <h3> SKLEPY: </h3>
                 <ul>
                   {
-                    shopsWithId.ingriedients.map(     //nazwa skladnika do poprawki w shopach
+                    shops.ingriedients.map(     //nazwe skladnika trzeba poprawic
                         ingriedients =>
                             <li key={ingriedients.id}>
                               {ingriedients.ingriedientId}
-                              <p key={shops.id}>{shops.find( ingriedients => ingriedients.ingriedientId === ingriedients.ingriedientId ).ingriedientPrice}</p>
+                              <p key={shops.id}>{shops.ingriedients.find( ingriedients => ingriedients.ingriedientId === ingriedients.ingriedientId ).ingriedientId}</p>
                             </li>
                     )
                   }
