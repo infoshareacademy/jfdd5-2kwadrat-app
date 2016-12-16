5353542725f736f2127186bbab87081093987a66
Kuba Melson <kubamelson@gmail.com>
2016-12-16 16:09:26 +0100

    Recipes with ingredients and amount

--- src/RecipeView/RecipeView.js
+++ src/RecipeView/RecipeView.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {Image, Col} from 'react-bootstrap'
 import {recipes} from '../data'
-
-
+import {ingredients} from '../data'
 
 
 export default (props) => {
@@ -10,11 +9,25 @@ export default (props) => {
     recipe => recipe.id === parseInt(props.params.recipeId, 10)
   )
   return (
-    <div key = {recipeWithId.id}>
+    <div key={recipeWithId.id}>
       <Col xs={12}>
         <h1>{recipeWithId.name}</h1>
         <Image src={recipeWithId.image}/>
-        <p>{recipeWithId.description}</p>
+        <p>{recipeWithId.description} </p>
+        <div>
+          <h3> SKŁADNIKI: </h3>
+          <ul>
+            {
+              recipeWithId.ingredients.map(
+                ingredient =>
+                  <li>
+                    {ingredient.id} {ingredient.ingredientAmount} {ingredient.unitMeasure}
+                    <p>{ingredients.find( item => item.id === ingredient.id ).name}</p>
+                  </li>
+              )
+            }
+          </ul>
+        </div>
       </Col>
     </div>
   )
