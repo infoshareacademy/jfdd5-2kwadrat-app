b3bc487de86666b373f4ef8ad38f8ab4dbc57dd7
JakubMelson <kubamelson@gmail.com>
2016-12-17 21:24:17 +0100

    no idea.

--- src/IngredientView/IngredientView.js
+++ src/IngredientView/IngredientView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ingredients} from '../data'
-import { shops } from '../data'
+import {shops} from '../data'
 import {Image, Col} from 'react-bootstrap'
 
 
@@ -8,27 +8,26 @@ export default (props) => {
   const ingredientsWithId = ingredients.find(
       ingredient => ingredient.id === parseInt(props.params.ingredientId, 10)
   )
+
   return (
       <div>
         <Col xs={6}>
-
           <div key={ingredientsWithId.id}>
             <Col xs={12}>
               <h1>{ingredientsWithId.name}</h1>
               <Image src={ingredientsWithId.img}/>
               <div>
                 <h3> SKLEPY: </h3>
-                {/*<ul>*/}
-                  {/*{*/}
-                    {/*shops.ingriedients.map(     //nazwe skladnika trzeba poprawic*/}
-                        {/*ingriedients =>*/}
-                            {/*<li key={ingriedients.id}>*/}
-                              {/*{ingriedients.ingriedientId}*/}
-                              {/*<p key={shops.id}>{shops.ingriedients.find( item => item.ingriedientId === ingriedients.ingriedientId ).name}</p>*/}
-                            {/*</li>*/}
-                    {/*)*/}
-                  {/*}*/}
-                {/*</ul>*/}
+
+                <ul>
+                  {
+
+                    shops.find(
+                        arrayOfIngredients => arrayOfIngredients.ingriedients.map(
+                        item => item.ingriedientId === ingredientsWithId.id).ingriedientId // :(
+                    )
+                  }
+                </ul>
               </div>
             </Col>
             {props.children}
@@ -38,4 +37,90 @@ export default (props) => {
 
       </div>
   )
+}
+//
+// <ul>
+//   {
+//     shopWithId.ingriedients.map(
+//         shopIngre =>
+//             ingredients.find(
+//                 ingre =>
+//                 ingre.id === shopIngre.ingriedientId
+//             )
+//     ).map(
+//         item =>
+//             <li key={item.id}>{item.name}</li>
+//     )
+//   }
+//
+// </ul>
+//
+// <ul>
+// {
+//   ingredientsWithId.ingriedients.map(     //nazwe skladnika trzeba poprawic
+//     ingriedients =>
+//
+//           ingriedients.find (
+//               ingr =>
+//               ingr.id === ingriedients.ingriedientId
+//           )
+//             ).map(
+//                 item =>
+//                     <li key={item.id}>{item.name}</li>
+//
+//
+// )
+// }
+// </ul>
+
+{/*{*/
+}
+{/*ingredientsWithId.map(     //nazwe skladnika trzeba poprawic*/
+}
+{/*ingriedients =>*/
+}
+
+{/*ingriedients.find (*/
+}
+{/*ingr =>*/
+}
+{/*ingr.id === ingriedients.ingriedientId*/
+}
+{/*)*/
+}
+{/*).map(*/
+}
+{/*item =>*/
+}
+{/*<li key={item.id}>{item.name}</li>*/
+}
+
+
+{/*)*/
+}
+{/*}*/
+}
+
+
+{/*<ul>*/
+}
+{/*{*/
+}
+{/*ingredientsWithId.ingriedients.map(     //nazwe skladnika trzeba poprawic*/
+}
+{/*ingriedients =>*/
+}
+{/*<li key={ingriedients.ingriedientId}>*/
+}
+{/*{ingriedients.ingriedientId}*/
+}
+{/*<p key={shops.id}>{shops.ingriedients.find( item => item.ingriedientId === ingriedients.ingriedientId ).name}</p>*/
+}
+{/*</li>*/
+}
+{/*)*/
+}
+{/*}*/
+}
+{/*</ul>*/
 }
