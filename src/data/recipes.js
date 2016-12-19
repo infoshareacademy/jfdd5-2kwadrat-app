5659301a05894b82bf16f3c217ab12b3ea434ea9
Kuba Melson <kubamelson@gmail.com>
2016-12-19 15:20:31 +0100

    Added some new reciepies

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -20,7 +20,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://4.cdn.katalogsmakow.pl/2014/12/05/96x96/zrazy.67275.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Lorem ipsum dolor sit amet dui lectus vulputate eu, aliquam arcu. Nulla ipsum at fermentum erat. ' +
     'Aenean ac massa augue, ullamcorper ac, vulputate at, velit. Suspendisse nulla ornare vitae, ' +
     'faucibus orci ac lacus. Cras turpis egestas.'
@@ -46,7 +46,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://4.cdn.katalogsmakow.pl/2014/10/05/96x96/zupa-pomidorowa.67719.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: '.Cras at lorem id mi. Pellentesque at diam tempor scelerisque, diam. ' +
     'Curabitur nec erat in faucibus orci luctus tellus vulputate accumsan. In lacinia ut, dignissim a, dolor.'
   },
@@ -71,7 +71,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://static.zmiksowani.pl/image/photo/2013/08/16/376109_45.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Ut molestie tristique mauris rhoncus wisi. Phasellus pulvinar mollis. ' +
     'Suspendisse fringilla viverra et, erat. Sed venenatis. Curabitur est dolor, varius ac, neque. ' +
     'Nam vestibulum id, dolor.'
@@ -97,7 +97,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://static.zmiksowani.pl/image/photo/2014/08/11/579156_45.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Praesent feugiat. Proin id nulla ac libero. Curabitur vitae metus. Proin sodales. ' +
     'Vivamus hendrerit dolor sed enim. Mauris viverra sit amet ipsum. Vestibulum leo.'
   },
@@ -122,7 +122,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://static.zmiksowani.pl/image/photo/2015/05/05/744101_45.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Maecenas vitae fermentum orci sit amet, ante. Vestibulum ante volutpat a, ultricies odio, ' +
     'a ligula. Aliquam posuere vitae, fringilla viverra sit amet eleifend posuere laoreet, nulla quis diam felis, ' +
     'vestibulum ac, euismod pulvinar, libero.'
@@ -148,7 +148,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://static.zmiksowani.pl/image/photo/2014/07/15/565747_45.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Duis sed est. Integer aliquam. In et ultrices posuere cubilia Curae, ' +
     'In hac habitasse platea dictumst. Quisque ut nibh nulla ligula nunc, accumsan adipiscing, ' +
     'risus sit amet eros. Ut sodales felis, volutpat eu, pede.'
@@ -169,7 +169,7 @@ export default [
         unitMeasure: 'gr'
       },
     ],
-    image: 'http://static.zmiksowani.pl/image/photo/2013/10/26/411610_45.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Vestibulum ante ipsum sit amet sapien facilisis sem condimentum nunc. Nulla mollis tempus. ' +
     'Quisque consectetuer dignissim, libero nec tellus. Proin venenatis vitae, dapibus a, blandit sed, dapibus diam.'
   },
@@ -194,7 +194,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://4.cdn.katalogsmakow.pl/2014/09/28/96x96/bigos.67443.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Etiam sapien eros, id erat sem et netus et netus et ultrices urna, id diam sit amet dui. ' +
     'In hac habitasse platea dictumst. Nunc felis. Nunc dictum quis, venenatis augue varius eu, commodo odio.'
   },
@@ -219,7 +219,7 @@ export default [
         unitMeasure: 'gr'
       }
     ],
-    image: 'http://dajprzepis.pl/photo/50x280/16662/wodzionka-zupa-kryzysowa.jpg',
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
     description: 'Cras tempus erat at magna. Mauris neque. Class aptent taciti sociosqu ad ' +
     'litora torquent per conubia nostra, per inceptos hymenaeos. Phasellus vitae sem.'
   },
@@ -240,6 +240,281 @@ export default [
       },
       {
         id: 20,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 11,
+    name: 'Kaparoczio',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 11,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 9,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 2,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 12,
+    name: 'Czarna wydma',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 19,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 1,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 7,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 13,
+    name: 'Alicante',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 8,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 3,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 7,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 14,
+    name: 'Ryba po śląsku',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 4,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 5,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 16,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 15,
+    name: 'Mazurskie kluski',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 13,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 20,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 21,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 16,
+    name: "Jedzenie a'la Foodtruck",
+    category: 'salad',
+    ingredients: [
+      {
+        id: 22,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 8,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 10,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 16,
+    name: 'Mroźna kraina',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 22,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 8,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 10,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 17,
+    name: 'Kabanosy w słoiku',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 1,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 3,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 5,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 18,
+    name: 'Som-tam mai ped',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 6,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 8,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 10,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 19,
+    name: 'Gulasz rycerski',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 13,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 5,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 2,
+        ingredientAmount: 450,
+        unitMeasure: 'gr'
+      }
+    ],
+    image: 'http://static.zmiksowani.pl/image/photo/2015/03/11/712688_45.jpg',
+    description: 'Maecenas eget est dolor, luctus nec, accumsan dictum, laoreet sit amet tempus arcu. Praesent feugiat. ' +
+    'Proin dui convallis nisl, a velit pede porta ligula. Phasellus purus. Pellentesque habitant morbi tristique sodales. '
+  },
+  {
+    id: 20,
+    name: 'Karmazynowy przypływ',
+    category: 'salad',
+    ingredients: [
+      {
+        id: 22,
+        ingredientAmount: 650,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 7,
+        ingredientAmount: 270,
+        unitMeasure: 'gr'
+      },
+      {
+        id: 7,
         ingredientAmount: 450,
         unitMeasure: 'gr'
       }
