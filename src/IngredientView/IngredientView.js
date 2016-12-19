5eb770c644fe66dd23897c4f5458896f5089c122
Kuba Melson <kubamelson@gmail.com>
2016-12-19 13:20:52 +0100

    It's working!

--- src/IngredientView/IngredientView.js
+++ src/IngredientView/IngredientView.js
@@ -10,36 +10,31 @@ export default (props) => {
   )
 
   return (
-      <div>
-        <Col xs={6}>
-          <div key={ingredientsWithId.id}>
-            <Col xs={12}>
-              <h1>{ingredientsWithId.name}</h1>
-              <Image src={ingredientsWithId.img}/>
-              <div>
-                <h3> SKLEPY: </h3>
-
-<<<<<<< HEAD
-                <ul>
-                  {
-                    shops.filter(
-                      shop => shop.ingredients.find(ingredient => ingredient.id === ingredientsWithId.id)
-                    ).map(
-                      shop =>
-                        <li key={shop.id}>
-                          {shop.name}
-                        </li>
-                    )
-                  }
-
-                </ul>
-              </div>
-            </Col>
-            {props.children}
-          </div>
-
-        </Col>
-
-      </div>
+    <div>
+      <Col xs={6}>
+        <div key={ingredientsWithId.id}>
+          <Col xs={12}>
+            <h1>{ingredientsWithId.name}</h1>
+            <Image src={ingredientsWithId.img}/>
+            <div>
+              <h3> SKLEPY: </h3>
+              <ul>
+                {
+                  shops.filter(
+                    shop => shop.ingredients.find(ingredient => ingredient.id === ingredientsWithId.id)
+                  ).map(
+                    shop =>
+                      <li key={shop.id}>
+                        {shop.name}
+                      </li>
+                  )
+                }
+              </ul>
+            </div>
+          </Col>
+          {props.children}
+        </div>
+      </Col>
+    </div>
   )
 }
