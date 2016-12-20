11f356b071de7aba5027946c0ac74f9aa85ae9ec
Dorota Leniec <dorleniec@gmail.com>
2016-12-20 17:02:34 +0100

    Displaying FilteredRecipesView

--- src/FilteredRecipesView/FilteredRecipesView.js
+++ src/FilteredRecipesView/FilteredRecipesView.js
@@ -1,18 +1,65 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
-import { recipes } from '../data'
+import {recipes} from '../data'
 
 const mapStateToProps = state => ({
     selectedIngredients: state.selectedIngredients.selectedIngredients
 })
 
+// const arrayOfIngredientsID = () =>
+//     recipes.map(
+//         recipe =>
+//             recipe.ingredients.map(
+//                 ingredient =>
+//                     ingredient.id
+//             )
+//     )
+//
+
 
 const FilteredRecipes = (props) => (
     <div>
-  <h1>DUPA</h1>
-      
-    </div>
-)
+        <h1>DUPA</h1>
+            <ul>
+            {
+                recipes.filter(
+                    recipe =>
+                        recipe.ingredients.every(
+                            ingredient =>
+                                props.selectedIngredients.find(
+                                    selected =>
+                                    selected.id === ingredient.id
+                                )
+                        )
+                ).map(
+                    recipe =>
+                        <li key={recipe.id}>
+                            mam wszystki skladniki!!!:{recipe.name}
+                        </li>
+                )}
 
-export default connect(mapStateToProps)(FilteredRecipes)
+            {
+                recipes.filter(
+                    recipe =>
+                        recipe.ingredients.find(
+                            ingredient =>
+                                props.selectedIngredients.find(
+                                    selected =>
+                                    selected.id === ingredient.id
+                                )
+                        )
+                ).map(
+                    recipe =>
+                        <li key={recipe.id}>
+                            mam co najmniej jeden: {recipe.name}
+                        </li>
+                )
+
+
+            }
+            </ul>
+            </div>
+            )
+
+            export default connect(mapStateToProps)(FilteredRecipes)
