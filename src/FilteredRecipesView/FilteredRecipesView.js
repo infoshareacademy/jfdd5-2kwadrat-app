ac4051fe79d90f3507f5f6b8679057a080e9ccff
Dorota Leniec <dorleniec@gmail.com>
2016-12-21 16:24:06 +0100

    adding new atribute to recipes array (arrayOfIngredients)

--- src/FilteredRecipesView/FilteredRecipesView.js
+++ src/FilteredRecipesView/FilteredRecipesView.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Image, Col} from 'react-bootstrap'
+import {Link} from 'react-router'
+
 
 import {recipes} from '../data'
 
@@ -7,21 +10,39 @@ const mapStateToProps = state => ({
     selectedIngredients: state.selectedIngredients.selectedIngredients
 })
 
-// const arrayOfIngredientsID = () =>
-//     recipes.map(
-//         recipe =>
-//             recipe.ingredients.map(
-//                 ingredient =>
-//                     ingredient.id
-//             )
-//     )
-//
+const arrayOfIngredientsID =
+    recipes.map(
+        recipe =>
+            recipe.ingredients.map(
+                ingredient =>
+                    ingredient.id
+            )
+    )
 
 
-const FilteredRecipes = (props) => (
-    <div>
-        <h1>DUPA</h1>
-            <ul>
+const FilteredRecipes = (props) => {
+
+    const arrayOfSelectedIngredientsID =
+        props.selectedIngredients.map(
+            selected =>
+                selected.id
+        )
+
+    recipes.map(
+        recipe => (
+        {
+            ...recipe,
+            arrayOfIngredients: recipe.ingredients.map(
+                ingredient =>
+                    arrayOfSelectedIngredientsID.indexOf(ingredient.id) !== -1 ? 1 : 0
+            )
+        }
+        )
+    )
+    return (
+        <div>
+            <h1>DUPA</h1>
+
             {
                 recipes.filter(
                     recipe =>
@@ -34,9 +55,15 @@ const FilteredRecipes = (props) => (
                         )
                 ).map(
                     recipe =>
-                        <li key={recipe.id}>
-                            mam wszystki skladniki!!!:{recipe.name}
-                        </li>
+                        <Col key={recipe.id} xs={12} sm={6} md={4}>
+                            <h1>
+                                MAM WSZYSTKIE SKLADNIKI
+                            </h1>
+                            <Link to={'/recipes/' + recipe.id}>
+                                <h2>{recipe.name}</h2>
+                                <Image src={recipe.image}/>
+                            </Link>
+                        </Col>
                 )}
 
             {
@@ -51,15 +78,20 @@ const FilteredRecipes = (props) => (
                         )
                 ).map(
                     recipe =>
-                        <li key={recipe.id}>
-                            mam co najmniej jeden: {recipe.name}
-                        </li>
+                        <Col key={recipe.id} xs={12} sm={6} md={4}>
+                            <h1>Mam kikla</h1>
+                            <Link to={'/recipes/' + recipe.id}>
+                                <h2>{recipe.name}</h2>
+                                <Image src={recipe.image}/>
+                            </Link>
+                        </Col>
                 )
 
 
             }
-            </ul>
-            </div>
-            )
 
-            export default connect(mapStateToProps)(FilteredRecipes)
+        </div>
+    )
+}
+
+export default connect(mapStateToProps)(FilteredRecipes)
