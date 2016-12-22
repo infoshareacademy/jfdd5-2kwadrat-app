dbf208596b7e6a690de26b9e018f85d922bc0f53
Dorota Leniec <dorleniec@gmail.com>
2016-12-22 16:09:23 +0100

    New Component ShopView created
    using bootstrap Component { Modal }

---
+++ src/AvailabilityView/ShopsLogoView/ShopView/ShopView.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import {ingredients} from '../../../data'
+import {connect} from 'react-redux'
+import {Modal, Button} from 'react-bootstrap'
+import {Link} from 'react-router'
+import {Image} from 'react-bootstrap'
+
+const mapStateToProps = state => ({
+    shops: state.shopsData.shops
+})
+
+const ShopView = React.createClass({
+
+    getInitialState() {
+        return {
+            showModal: false,
+            shop: {
+                ingredients: []
+            },
+
+        }
+    },
+
+    close() {
+        this.setState({
+            showModal: false,
+        })
+    },
+
+    open(thisShop) {
+        this.setState({
+                showModal: true,
+                shop: thisShop
+            },
+        )
+    },
+
+    render(){
+        return (
+            <div>
+                <Link key={this.props.shop.name}
+                      onClick={
+                          () =>
+                          this.open(this.props.shop)
+                      }
+                >
+                    <Image src={this.props.shop.logo}
+                           height="100px"
+                           width="auto"
+                           rounded
+                    />
+                </Link>
+                < Modal show={this.state.showModal}
+                        onHide={this.close}>
+
+                    <Modal.Header closeButton>
+
+                        <Modal.Title >
+                            {this.state.shop.name}
+                        </Modal.Title>
+
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Image src={this.state.shop.logo}
+                               height="100px"
+                               width="auto"
+                               rounded
+                        />
+                        <ul key={this.props.shop.id}>
+                            {
+                                this.state.shop.ingredients.map(
+                                    shopIngredients =>
+                                        ingredients.find(
+                                            ingredient =>
+                                            ingredient.id === shopIngredients.id
+                                        )
+                                ).map(
+                                    item =>
+                                        <li key={item.id}>
+                                            {item.name}
+                                            <Image src={item.img}
+                                                   height="40px"
+                                                   width="auto"
+                                                   rounded
+                                            /></li>
+                                )
+                            }
+                        </ul>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+})
+
+export default connect(mapStateToProps)(ShopView)
--- src/AvailabilityView/ShopsLogoView/ShopsLogoView.js
+++ src/AvailabilityView/ShopsLogoView/ShopsLogoView.js
@@ -1,111 +1,50 @@
 import React from 'react'
-import {Image, Col} from 'react-bootstrap'
-import {Link} from 'react-router'
-import {Modal, Button} from 'react-bootstrap'
+import {Col} from 'react-bootstrap'
+
 import {connect} from 'react-redux'
 import {default as ShopsMap} from './ShopsMap/ShopsMap'
+import {default as ShopView} from './ShopView/ShopView'
 
-
-import {ingredients} from '../../data'
 import './ShopLogoView.css'
 
 const mapStateToProps = state => ({
     shops: state.shopsData.shops
 })
 
-const ShopsLogoView = React.createClass({
-
-    getInitialState() {
-        return {
-            showModal: false,
-            shop: {
-                ingredients: []
-            },
-
-        }
-    },
-
-    close() {
-        this.setState({
-            showModal: false,
-        })
-    },
-
-    open(thisShop) {
-        this.setState({
-                showModal: true,
-                shop: thisShop
-            },
-        )
-    },
-
-    render() {
-        return (
+const ShopsLogoView = (props) => {
+    return (
+        <div>
             <div>
-                <div>
-                    <h1>Shops: </h1>
-                    {
-                        this.props.shops.reduce(
-                            (prev, next) => prev.find(item => item.name === next.name) ? prev : prev.concat(next),
-                            []
-                        ).map(
-                            shop => {
-                                return (
-                                    <Col key={shop.id} xs={12} sm={6} md={4} className='shop-container'>
-                                        <Link key={shop.name}
-                                              onClick={() => this.open(shop)
-                                              }
-                                        > <Image src={shop.logo}
-                                                 height="100px"
-                                                 width="auto" rounded
-                                        />
-                                        </Link>
-                                        < Modal show={this.state.showModal} onHide={this.close}>
-                                            <Modal.Header closeButton>
-                                                <Modal.Title >{this.state.shop.name}</Modal.Title>
-                                            </Modal.Header>
-                                            <Modal.Body>
-                                                <Image src={this.state.shop.logo}
-                                                       height="100px"
-                                                       width="auto" rounded
-                                                />
-                                                <ul key={shop.id}>
-                                                    {
-                                                        this.state.shop.ingredients.map(
-                                                            shopIngredients =>
-                                                                ingredients.find(
-                                                                    ingredient =>
-                                                                    ingredient.id === shopIngredients.id
-                                                                )
-                                                        ).map(
-                                                            item =>
-                                                                <li key={item.id}>{item.name}
-                                                                    <Image src={item.img}
-                                                                           height="40px"
-                                                                           width="auto" rounded
-                                                                    /></li>
-                                                        )
-                                                    }
-                                                </ul>
-                                            </Modal.Body>
-                                            <Modal.Footer>
-                                                <Button onClick={this.close}>Close</Button>
-                                            </Modal.Footer>
-                                        </Modal>
-                                    </Col>)
-                            }
-                        )
-
-                    }
-
-                </div>
-                <br/>
-                <Col xs={12}>
-                    {<ShopsMap />}
-                </Col>
+                <h1>Shops: </h1>
+                {
+                    props.shops.reduce(
+                        (prev, next) =>
+                            prev.find(
+                                item =>
+                                item.name === next.name) ?
+                                prev :
+                                prev.concat(next),
+                        []
+                    ).map(
+                        shop => {
+                            return (
+                                <Col key={shop.id}
+                                     xs={12}
+                                     sm={6}
+                                     md={4}
+                                     className='shop-container'>
+                                    <ShopView shop={shop}/>
+                                </Col>
+                            )
+                        }
+                    )
+                }
             </div>
-        )
-    }
-})
+            <Col xs={12}>
+                {<ShopsMap />}
+            </Col>
+        </div>
+    )
+}
 
 export default connect(mapStateToProps)(ShopsLogoView)
