7c474eaefeed393a95a18054f04d573bc520a75c
Dorota Leniec <dorleniec@gmail.com>
2016-12-22 15:42:26 +0100

    new shops in data

--- src/data/shops.js
+++ src/data/shops.js
@@ -3,8 +3,8 @@ export default [
         id: 1,
         name: 'biedronka',
         location: {
-            lat: 54.403003,
-            lng: 18.572261
+            lat: 54.407103,
+            lng: 18.591312
         },
         logo: 'http://www.webtailor.pl/cms/img/u/case/biedronka_logo.png',
         ingredients: [
@@ -24,6 +24,75 @@ export default [
     },
     {
         id: 2,
+        name: 'biedronka',
+        location: {
+            lat: 54.412498,
+            lng: 18.590969
+        },
+        logo: 'http://www.webtailor.pl/cms/img/u/case/biedronka_logo.png',
+        ingredients: [
+            {
+                id: 1,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 2,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 3,
+                ingredientPrice: 3.04
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'biedronka',
+        location: {
+            lat: 54.406826,
+            lng: 18.591389
+        },
+        logo: 'http://www.webtailor.pl/cms/img/u/case/biedronka_logo.png',
+        ingredients: [
+            {
+                id: 1,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 2,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 3,
+                ingredientPrice: 3.04
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: 'biedronka',
+        location: {
+            lat: 54.406173,
+            lng: 18.613232
+        },
+        logo: 'http://www.webtailor.pl/cms/img/u/case/biedronka_logo.png',
+        ingredients: [
+            {
+                id: 1,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 2,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 3,
+                ingredientPrice: 3.04
+            }
+        ]
+    },
+    {
+        id: 5,
         name: 'lidl',
         location: {
             lat: 54.390724,
@@ -42,7 +111,7 @@ export default [
         ]
     },
     {
-        id: 3,
+        id: 6,
         name: 'lidl',
         location: {
             lat: 54.393323,
@@ -61,7 +130,7 @@ export default [
         ]
     },
     {
-        id: 4,
+        id: 7,
         name: 'lidl',
         location: {
             lat: 54.389425,
@@ -80,7 +149,7 @@ export default [
         ]
     },
     {
-        id: 5,
+        id: 8,
         name: 'tesco',
         location: {
             lat: 52.403003,
@@ -107,7 +176,7 @@ export default [
         ]
     },
     {
-        id: 6,
+        id: 9,
         name: 'tesco',
         location: {
             lat: 52.403003,
@@ -134,11 +203,11 @@ export default [
         ]
     },
     {
-        id: 7,
+        id: 10,
         name: 'stokrotka',
         location: {
-            lat: 54.453003,
-            lng: 18.62261
+            lat: 54.302211,
+            lng: 18.577375
         },
         logo: 'http://www.lublinplaza.pl/_cache/shops/510-255/fill/stokrotka.png',
         ingredients: [
@@ -169,11 +238,46 @@ export default [
         ]
     },
     {
-        id: 8,
+        id: 11,
+        name: 'stokrotka',
+        location: {
+            lat: 54.601611,
+            lng: 18.184521
+        },
+        logo: 'http://www.lublinplaza.pl/_cache/shops/510-255/fill/stokrotka.png',
+        ingredients: [
+            {
+                id: 1,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 5,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 1,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 2,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 3,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 5,
+                ingredientPrice: 3.04
+            }
+        ]
+    },
+    {
+        id: 12,
         name: 'polo-market',
         location: {
-            lat: 54.203003,
-            lng: 18.972261
+            lat: 54.403079,
+            lng: 18.587018
         },
         logo: 'http://rabatuj.pl/r/i/r/images/polomarket_logo.png',
         ingredients: [
@@ -192,11 +296,105 @@ export default [
         ]
     },
     {
-        id: 9,
+        id: 13,
+        name: 'polo-market',
+        location: {
+            lat: 54.601611,
+            lng: 18.184521
+        },
+        logo: 'http://rabatuj.pl/r/i/r/images/polomarket_logo.png',
+        ingredients: [
+            {
+                id: 1,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 4,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 6,
+                ingredientPrice: 3.04
+            }
+        ]
+    },
+
+    {
+        id: 14,
         name: 'żabka',
         location: {
-            lat: 52.403003,
-            lng: 14.572261
+            lat: 54.411315,
+            lng: 18.550564
+        },
+        logo: 'https://zdjecia.pl.sftcdn.net/pl/scrn/69690000/69690312/image-05-535x535.png',
+        ingredients: [
+            {
+                id: 9,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 10,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 11,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 12,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 13,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 14,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 15,
+                ingredientPrice: 2.04
+            },
+            {
+                id: 16,
+                ingredientPrice: 4.84
+            },
+            {
+                id: 17,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 18,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 19,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 20,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 21,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 22,
+                ingredientPrice: 3.04
+            },
+            {
+                id: 23,
+                ingredientPrice: 3.04
+            }
+        ]
+    }, {
+        id: 15,
+        name: 'żabka',
+        location: {
+            lat: 54.412713,
+            lng: 18.601033
         },
         logo: 'https://zdjecia.pl.sftcdn.net/pl/scrn/69690000/69690312/image-05-535x535.png',
         ingredients: [
