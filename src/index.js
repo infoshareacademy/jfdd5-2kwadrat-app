980c3bce8ff5a4dbe698b34a12ebd6e601052f05
Dorota Leniec <dorleniec@gmail.com>
2016-12-22 13:31:03 +0100

    Modal backdrop background opacity changed

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ import {FilteredRecipesView} from './FilteredRecipesView'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap-theme.css'
-
 import './index.css'
 
 ReactDOM.render(
