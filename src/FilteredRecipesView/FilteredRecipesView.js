c6f5359a0a11f2da4ce8094956f646befd46d684
Dorota <dorleniec@gmail.com>
2017-01-03 16:21:06 +0100

    FilteredRecipesView new display (as in AllRecipesView) new sorting missing ingredients number display number if ingredients you have display

--- src/FilteredRecipesView/FilteredRecipesView.js
+++ src/FilteredRecipesView/FilteredRecipesView.js
@@ -1,99 +1,98 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Image, Col} from 'react-bootstrap'
+import {Image, Col, Button} from 'react-bootstrap'
 import {Link} from 'react-router'
+import './FilteredRecipesView.css'
 
 
 import {recipes} from '../data'
 
 const mapStateToProps = state => ({
-    selectedIngredients: state.selectedIngredients.selectedIngredients
+  selectedIngredients: state.selectedIngredients.selectedIngredients
 })
-
-const arrayOfIngredientsID =
-    recipes.map(
-        recipe =>
-            recipe.ingredients.map(
-                ingredient =>
-                    ingredient.id
-            )
-    )
-
 
 const FilteredRecipes = (props) => {
 
-    const arrayOfSelectedIngredientsID =
-        props.selectedIngredients.map(
-            selected =>
-                selected.id
-        )
-
-    const newRecipesArray = recipes.map(
-        recipe => (
-        {
-            ...recipe,
-            numberOfFittedIngredients: parseInt(recipe.ingredients.map(
-                ingredient =>
-                    arrayOfSelectedIngredientsID.indexOf(ingredient.id) !== -1 ? 1 : 0
-            ).reduce((prev, next)=>
-                prev + next
-                , 0))
-        }
-        )
+  const arrayOfSelectedIngredientsID =
+    props.selectedIngredients.map(
+      selected =>
+        selected.id
     )
-    return (
 
-        <div>
-          <Link to={'/'}><button>Wstecz</button></Link>
-            <h1>DUPA</h1>
-            <h3>{console.log(newRecipesArray)}
-                {
-                    newRecipesArray.filter(
-                        recipe =>
-                        recipe.numberOfFittedIngredients === 3
-                    ).map(
-                        recipe =>(
-                            <p> Mam wszystkie skladniki:
-                                { <Link to={'/recipes/' + recipe.id}>
-                                    <h2>{recipe.name}</h2>
-                                    <Image src={recipe.image}/>
-                                </Link>}</p>
-                        )
-                    )
-                }
-
-                {
-                    newRecipesArray.filter(
-                        recipe =>
-                        recipe.numberOfFittedIngredients === 2
-                    ).map(
-                        recipe =>(
-                            <p key={recipe.id}> Mam kilka(2) skladniki:
-                                { <Link to={'/recipes/' + recipe.id}>
-                                    <h2>{recipe.name}</h2>
-                                    <Image src={recipe.image}/>
-                                </Link>}</p>
-                        )
-                    )
-                }
-
-                {
-                    newRecipesArray.filter(
-                        recipe =>
-                        recipe.numberOfFittedIngredients === 1
-                    ).map(
-                        recipe =>(
-                            <p> Mam mało skladniki:
-                                { <Link to={'/recipes/' + recipe.id}>
-                                    <h2>{recipe.name}</h2>
-                                    <Image src={recipe.image}/>
-                                </Link>}</p>
-                        )
-                    )
-                }
-            </h3>
-        </div>
+  const newRecipesArray = recipes.map(
+    recipe => (
+      {
+        ...recipe,
+        numberOfFittedIngredients: recipe.ingredients.map(
+          ingredient =>
+            arrayOfSelectedIngredientsID.indexOf(ingredient.id) !== -1 ? 1 : 0
+        ).filter(
+          item =>
+          item === 1
+        )
+      }
     )
+  )
+  newRecipesArray.sort((a, b) =>
+    a.ingredients.length - b.ingredients.length
+  )
+  newRecipesArray.sort(
+    (a, b) =>
+    (a.ingredients.length - a.numberOfFittedIngredients.length) - ( b.ingredients.length - b.numberOfFittedIngredients.length)
+  )
+
+  console.log(newRecipesArray)
+
+  return (
+    <div>
+      <div className="button-back">
+        <Link to={'/'}><Button>Wstecz</Button></Link>
+      </div>
+      {
+        arrayOfSelectedIngredientsID.length !== 0 ?
+          newRecipesArray.map(
+            recipe => (
+              <Link key={recipe.id} to={'/recipes/' + recipe.id}>
+                <Col key={recipe.id} xs={12} sm={6} md={4}>
+                  <div className="recipeCard recipeCardHeight">
+                    <Image className="photo image" src={recipe.image}/>
+                    <h2>{recipe.name}</h2>
+                    { recipe.numberOfFittedIngredients.length === recipe.ingredients.length ?
+                      <p className="missing-ingredients-info">
+                        Masz wszystkie składniki! Do dzieła
+                      </p> :
+
+                      <p className="missing-ingredients-info">
+                        Masz {recipe.numberOfFittedIngredients.length}
+                        {recipe.numberOfFittedIngredients.length === 1 ?
+                          ' składnik' :
+                          recipe.numberOfFittedIngredients.length === 0 || recipe.numberOfFittedIngredients.length > 4 ?
+                            ' składników' :
+                            ' składniki'
+                        }
+                      </p>
+                    }
+                    {recipe.numberOfFittedIngredients.length === recipe.ingredients.length ?
+                      '' :
+                      <p className="missing-ingredients-info">
+                        Brakuje Ci {recipe.ingredients.length - recipe.numberOfFittedIngredients.length }
+                        skladników
+                      </p>
+                    }
+
+                    <div className="icons">
+                      <div className="recipeTime">{recipe.time}</div>
+                      <div className="recipeDifficult">{recipe.difficult}</div>
+                    </div>
+                  </div>
+                </Col>
+              </Link>
+            )
+          ) :
+          <h1>Pokaż co masz w lodówce ; )</h1>
+      }
+    </div>
+  )
 }
 
 export default connect(mapStateToProps)(FilteredRecipes)
