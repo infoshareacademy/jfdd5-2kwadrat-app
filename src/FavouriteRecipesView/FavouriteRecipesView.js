c760283b941e044f3900a99b42705187f0b26d7c
dorota <dorleniec@gmail.com>
2017-01-10 23:54:14 +0100

    passing userData to store as loggedUserData

--- src/FavouriteRecipesView/FavouriteRecipesView.js
+++ src/FavouriteRecipesView/FavouriteRecipesView.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import recipes from '../data/recipes'
+
 const mapStateToProps = state => ({
-  userId: state.loggedInData.loggedInUserId
+  userId: state.loggedInData.loggedInUserId,
+  user: state.loggedInData.loggedUserData
 })
 
 
@@ -12,9 +15,7 @@ const mapStateToProps = state => ({
     {
       typeof props.userId === 'number' ?
         <p>Przepisy:
-          {
 
-          }
         </p> :
         <h1>Zaloduj się by wyświetlić listę</h1>
     }
