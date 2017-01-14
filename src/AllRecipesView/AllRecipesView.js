391a3cbedcf969920a14a0e70d9cbb63a9848b67
JakubMelson <kubamelson@gmail.com>
2017-01-14 19:56:14 +0100

    AllRecipesView Smooth enter animation

--- src/AllRecipesView/AllRecipesView.js
+++ src/AllRecipesView/AllRecipesView.js
@@ -3,6 +3,7 @@ import './AllRecipesViewStyles.css'
 import {recipes} from '../data'
 import {Image, Col} from 'react-bootstrap'
 import {Link} from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default (props) => (
   <div className="wrapper" >
@@ -12,6 +13,11 @@ export default (props) => (
           return (
 
             <Col key={recipe.id} xs={12} sm={6} md={4}>
+              <ReactCSSTransitionGroup
+                  transitionName="route"
+                  transitionEnterTimeout={100}
+                  transitionAppearTimeout={100}
+                  transitionAppear={true}>
               <div className="recipeCard">
                 <Link to={'/recipes/' + recipe.id}>
                   <Image className="photo image" src={recipe.image}/>
@@ -23,6 +29,7 @@ export default (props) => (
                   <div className="recipeDifficult">{recipe.difficult}</div>
                 </div>
               </div>
+              </ReactCSSTransitionGroup>
             </Col>
           )
         }
