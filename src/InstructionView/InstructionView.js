0df8580c24ede64c9d4382dfc5c4b720e033d68d
JakubMelson <kubamelson@gmail.com>
2017-01-16 23:31:54 +0100

    First animation that works the way I wanted... :)

--- src/InstructionView/InstructionView.js
+++ src/InstructionView/InstructionView.js
@@ -12,19 +12,29 @@ export default () => (
         transitionAppearTimeout={500}
         transitionAppear={true}>
   <div className="instruction-view">
-    <Col xs={4} className="instruction-image">
+    <ReactCSSTransitionGroup
+        transitionName="slideInLeft"
+        transitionAppearTimeout={500}
+        transitionAppear={true}>
+    <Col key="ssss" xs={4} className="instruction-image">
       <img src={process.env.PUBLIC_URL + '/images/fridge-512.png'}
            alt="lodowka"
            height={240}
       />
     </Col>
+    </ReactCSSTransitionGroup>
 
+    <ReactCSSTransitionGroup
+        transitionName="slideInRight"
+        transitionAppearTimeout={500}
+        transitionAppear={true}>
     <Col xs={6}>
       <h2 className="instruction-text">
         Wypełnij <Link to={'/form'}><span className="span-button">formularz</span></Link>, a nasza wyszukiwarka znajdzie<br/>
         <Link to={'/filtered-recipes'}><span className="span-button">Przepisy dla Ciebie</span></Link>.
       </h2>
     </Col>
+    </ReactCSSTransitionGroup>
   </div>
     </ReactCSSTransitionGroup>
 )
