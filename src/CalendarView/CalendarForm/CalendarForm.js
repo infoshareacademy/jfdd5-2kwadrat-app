45d14bc72a4f16eaf703d2568d8491046e0ef18f
Dorota Leniec <dorleniec@gmail.com>
2017-01-16 16:31:03 +0100

    DropDwon Button with recipes names

--- src/CalendarView/CalendarForm/CalendarForm.js
+++ src/CalendarView/CalendarForm/CalendarForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {DropdownButton, MenuItem, Button} from 'react-bootstrap'
 
+import recipes from '../../data/recipes'
+
 class CalendarForm extends React.Component {
 
     render() {
@@ -10,6 +12,21 @@ class CalendarForm extends React.Component {
                 <DropdownButton title="Trudność:" id="bg-vertical-dropdown-2">
                     <MenuItem eventKey="1">Hard</MenuItem>
                     <MenuItem eventKey="2">Light</MenuItem>
+                </DropdownButton>
+
+                <DropdownButton title="Trudność:" id="bg-vertical-dropdown-2">
+                    <MenuItem eventKey="1">Hard</MenuItem>
+                    <MenuItem eventKey="2">Light</MenuItem>
+                </DropdownButton>
+
+                <DropdownButton title="Przepis:" id="bg-vertical-dropdown-2">
+                    {recipes.map(
+                        recipe =>
+                            <MenuItem eventKey={recipe.id}>{recipe.name}</MenuItem>
+                    )}
+
+                    <MenuItem eventKey="1">Hard</MenuItem>
+
                 </DropdownButton>
             </div>
         )
