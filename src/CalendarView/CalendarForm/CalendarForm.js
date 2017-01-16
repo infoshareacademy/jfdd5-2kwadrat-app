2dbceaf99c80801f7bebdb14a4f36f2c39f3964f
Dorota Leniec <dorleniec@gmail.com>
2017-01-16 16:59:26 +0100

    corrections code review

--- src/CalendarView/CalendarForm/CalendarForm.js
+++ src/CalendarView/CalendarForm/CalendarForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {DropdownButton, MenuItem, Button, FormControl, FormGroup,ControlLabel} from 'react-bootstrap'
+import {DropdownButton, MenuItem, FormControl, FormGroup, ControlLabel} from 'react-bootstrap'
 
 import '../CalendarView.css'
 import recipes from '../../data/recipes'
@@ -7,9 +7,9 @@ import recipes from '../../data/recipes'
 class CalendarForm extends React.Component {
 
     render() {
-        return(
+        return (
             <div style={{height: 500}}>
-               <h1>Calendar ZUPA</h1>
+                <h1>Calendar ZUPA</h1>
                 <DropdownButton title="Trudność:" id="bg-vertical-dropdown-2">
                     <MenuItem eventKey="1">Hard</MenuItem>
                     <MenuItem eventKey="2">Light</MenuItem>
@@ -18,29 +18,18 @@ class CalendarForm extends React.Component {
                 <DropdownButton title="Trudność:" id="bg-vertical-dropdown-2">
                     <MenuItem eventKey="1">Hard</MenuItem>
                     <MenuItem eventKey="2">Light</MenuItem>
-                </DropdownButton>
-
-                <DropdownButton title="Przepis:" id="bg-vertical-dropdown-2">
-                    {recipes.map(
-                        recipe =>
-                            <MenuItem eventKey={recipe.id}>{recipe.name}</MenuItem>
-                    )}
-
-                    <MenuItem eventKey="1">Hard</MenuItem>
-
                 </DropdownButton>
 
                 <FormGroup controlId="formControlsSelect">
                     <ControlLabel>Wybierz danie:</ControlLabel>
-                    <FormControl componentClass="select" placeholder="select" onChange={(data) => console.log(data.target.value)}>
+                    <FormControl componentClass="select" placeholder="select"
+                                 onChange={(data) => console.log(data.target.value)}>
                         {
                             recipes.map(
-                            recipe =>
-                                <option  value={recipe.name} key={recipe.name}>{recipe.name}</option>
-                        )
+                                recipe =>
+                                    <option value={recipe.name} key={recipe.name}>{recipe.name}</option>
+                            )
                         }
-
-
                     </FormControl>
                 </FormGroup>
             </div>
