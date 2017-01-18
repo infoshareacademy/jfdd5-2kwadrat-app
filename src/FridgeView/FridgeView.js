abae94097421428896dbc744c9927e75a9d9dd9f
Maciej Gerka <maciej.gerka@gmail.com>
2017-01-18 10:26:58 +0100

    FridgeView styling

--- src/FridgeView/FridgeView.js
+++ src/FridgeView/FridgeView.js
@@ -4,7 +4,7 @@ import {ingredients} from '../data'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 
-import {Image, Col, Row, FormControl, Button} from 'react-bootstrap'
+import {Image, Col, FormControl, Button} from 'react-bootstrap'
 import {Link} from 'react-router'
 import {connect} from 'react-redux'
 import FaCutlery from 'react-icons/lib/fa/cutlery'
@@ -47,136 +47,119 @@ class FridgeView extends React.Component {
         transitionName="fadeFridgeView"
         transitionAppearTimeout={400}
         transitionAppear={true}>
-        <div className="divKeepsAll">
-          <form onSubmit={this.handleSubmit}>
-            <Col className="ingredientInputRow" xs={12}>
-              <h2 className="titles titleTop">Wybierz co masz w lodówce</h2>
+        <form onSubmit={this.handleSubmit}>
+          <Col className="ingredientInputRow" xs={12}>
+            <h2 className="titles">Wybierz co masz w lodówce</h2>
 
-              <FormControl
-                className="ingredientInput"
-                bsSize="sm"
-                type="text"
-                placeholder="w lodówce mam..."
-                onChange={(event) => this.setState({search: event.target.value})}
-              />
-
-              {ingredients.filter(
-                ingredient => this.state.search === '' ? false : ingredient.name.includes(this.state.search)
-              ).slice(0, 4).map(
-                ingredient => {
-                  return (
-                    <div onClick={
-                      () => {
-                        if (this.props.selectedIngredients.find(item => item.id === ingredient.id) === undefined) {
-                          this.setState(
-                            {
-                              ingredients: this.state.ingredients.concat(
-                                {
-                                  name: ingredient.name,
-                                  id: ingredient.id,
-                                  img: ingredient.img
-                                })
-                            },
-                            () => this.props.addIngredient(ingredient)
-                          )
-                        } else {
-                          this.setState(
-                            {
-                              ingredients: this.state.ingredients.filter(
-                                item => item.id !== ingredient.id
-                              )
-                            },
-                            () => this.props.removeIngredient(ingredient.id)
-                          )
-                        }
+            <FormControl
+              className="ingredientInput"
+              bsSize="sm"
+              type="text"
+              placeholder="w lodówce mam..."
+              onChange={(event) => this.setState({search: event.target.value})}
+            />
+            {ingredients.filter(
+              ingredient => this.state.search === '' ? false : ingredient.name.includes(this.state.search)
+            ).slice(0, 4).map(
+              ingredient => {
+                return (
+                  <div onClick={
+                    () => {
+                      if (this.props.selectedIngredients.find(item => item.id === ingredient.id) === undefined) {
+                        this.setState(
+                          {
+                            ingredients: this.state.ingredients.concat(
+                              {
+                                name: ingredient.name,
+                                id: ingredient.id,
+                                img: ingredient.img
+                              })
+                          },
+                          () => this.props.addIngredient(ingredient)
+                        )
+                      } else {
+                        this.setState(
+                          {
+                            ingredients: this.state.ingredients.filter(
+                              item => item.id !== ingredient.id
+                            )
+                          },
+                          () => this.props.removeIngredient(ingredient.id)
+                        )
                       }
-                    }>
-                      <div className="ingredientFieldContent filtered">
-                        <Col xs={4}>
-                          <Image className="filteredIngredientImage" src={ingredient.img}/>
-                        </Col>
+                    }
+                  }>
+                    <div className="ingredientFieldContent filtered">
+                      <Col xs={4}>
+                        <Image className="filteredIngredientImage" src={ingredient.img}/>
+                      </Col>
 
-                        <Col xs={8}>
-                          <div className="ingredientName">{ingredient.name}</div>
-                        </Col>
-                      </div>
+                      <Col xs={8}>
+                        <div className="ingredientName">{ingredient.name}</div>
+                      </Col>
                     </div>
-                  )
-                }
-              )}
-            </Col>
-          </form>
-
-          <form className="formBottomHalf">
-            <Col xs={12}>
-              {this.props.selectedIngredients.length === 0 ? null :
-                <hr className="middleDividingLine"></hr>}
-              {/*</Col>*/}
-              {/*<Col xs={12}>*/}
-              {this.props.selectedIngredients.length === 0 ? null :
-                <h2 className="titles">Wybrane produkty:</h2>}
-              {/*</Col>*/}
-
-              {/*<Row>*/}
-              {
-                this.props.selectedIngredients.map(
-                  ingredient =>
-                    <div key={ingredient.id} onClick={
-                      () => {
-                        if (this.props.removeIngredient(ingredient.id)) {
-                          this.setState(
-                            {
-                              ingredients: this.state.ingredients.filter(
-                                item => item.id !== ingredient.id
-                              )
-                            },
-                            () => this.props.removeIngredient(ingredient.id)
-                          )
-                        }
-                      }
-                    }>
-                      <div className="ingredientFieldContent chosen">
-                        <Col xs={4}>
-                          <Image className="chosenIngredientImage" src={ingredient.img}/>
-                        </Col>
-                        <Col xs={8}>
-                          <div className="ingredientName">{ingredient.name}</div>
-                        </Col>
-                      </div>
-                    </div>
+                  </div>
                 )
               }
-              {/*</Row>*/}
-            </Col>
+            )}
+          </Col>
+        </form>
 
-            {/*<Row>*/}
-            <Col xs={12}>
-              {this.props.selectedIngredients.length === 0 ? null :
-                <hr className="bottomDividingLine"></hr>}
-            </Col>
-            {/*</Row>*/}
-          </form>
+        <Col className="formBottomHalf" xs={12}>
+          {this.props.selectedIngredients.length === 0 ? null :
+            <hr className="middleDividingLine"></hr>}
+          {this.props.selectedIngredients.length === 0 ? null :
+            <h2 className="titles">Wybrane produkty:</h2>}
 
-          <div className="searchButton">
-            {this.props.selectedIngredients.length === 0 ? null :
+          {
+            this.props.selectedIngredients.map(
+              ingredient =>
+                <div key={ingredient.id} onClick={
+                  () => {
+                    if (this.props.removeIngredient(ingredient.id)) {
+                      this.setState(
+                        {
+                          ingredients: this.state.ingredients.filter(
+                            item => item.id !== ingredient.id
+                          )
+                        },
+                        () => this.props.removeIngredient(ingredient.id)
+                      )
+                    }
+                  }
+                }>
+                  <div className="ingredientFieldContent chosen">
+                    <Col xs={4}>
+                      <Image className="chosenIngredientImage" src={ingredient.img}/>
+                    </Col>
+                    <Col xs={8}>
+                      <div className="ingredientName">{ingredient.name}</div>
+                    </Col>
+                  </div>
+                </div>
+            )
+          }
+          {this.props.selectedIngredients.length === 0 ? null :
+            <hr className="bottomDividingLine"></hr>}
+        </Col>
 
 
-              //na onClick wysyłam informację do stora że lista jest gotowa;
-              // w reducerze wpisuję nowy stan;
-              // w FilteredRecipesView zamieniam ternary operator na to czy lista jest w sorze czy nie
-              <div className="searchButtonText">
-                <Link to={'/filtered-recipes'}>
-                  <Button bsStyle="warning"
-                          bsSize="large">
-                    <FaCutlery /> PRZEPISY DLA CIEBIE
-                  </Button>
-                </Link>
-              </div>}
-          </div>
+        {/*//na onClick wysyłam informację do stora że lista jest gotowa;*/}
+        {/*// w reducerze wpisuję nowy stan;*/}
+        {/*// w FilteredRecipesView zamieniam ternary operator na to czy lista jest w sorze czy nie*/}
+        <div>
+          {this.props.selectedIngredients.length === 0 ? null :
+            <Link to={'/filtered-recipes'}>
+              <Button
+                className="searchButton"
+                bsStyle="warning"
+                bsSize="large">
+                <FaCutlery /> PRZEPISY DLA CIEBIE
+              </Button>
+            </Link>}
         </div>
       </ReactCSSTransitionGroup>
     )
-
   }
 }
 
