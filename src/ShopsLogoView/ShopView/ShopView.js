2d53c4da3b0d8d35c9b3652895b269d763451ce1
JakubMelson <kubamelson@gmail.com>
2017-01-21 18:01:28 +0100

    ShopView changes

--- src/ShopsLogoView/ShopView/ShopView.js
+++ src/ShopsLogoView/ShopView/ShopView.js
@@ -7,7 +7,6 @@ import {Image} from 'react-bootstrap'
 import GoChecklist from 'react-icons/lib/go/checklist'
 
 
-
 const mapStateToProps = state => ({
   shops: state.shopsData.shops
 })
@@ -20,7 +19,6 @@ const ShopView = React.createClass({
       shop: {
         ingredients: []
       },
-
     }
   },
 
@@ -41,92 +39,92 @@ const ShopView = React.createClass({
 
   render(){
     return (
-      <div>
-        <Link key={this.props.shop.name}
-              onClick={
-                () =>
-                  this.open(this.props.shop)
-              }
-        >
-          <Image src={this.props.shop.logo}
-                 height={this.props.height}
-                 width="auto"
-                 rounded
-                 className="shop-logo"
-          />
-        </Link>
-        <Modal show={this.state.showModal}
-                onHide={this.close}>
+        <div>
+          <Link key={this.props.shop.name}
+                onClick={
+                  () =>
+                      this.open(this.props.shop)
+                }
+          >
+            <Image src={this.props.shop.logo}
+                   height={this.props.height}
+                   width="auto"
+                   rounded
+                   className="shop-logo"
+            />
+          </Link>
+          <Modal show={this.state.showModal}
+                 onHide={this.close}>
 
-          <Modal.Header closeButton>
+            <Modal.Header closeButton>
 
-            <Modal.Title >
-              <Image src={this.state.shop.logo}
-                     height='50px'
-                     width="auto"
-                     rounded
-              />
+              <Modal.Title >
+                <Image src={this.state.shop.logo}
+                       height='50px'
+                       width="auto"
+                       rounded
+                />
 
-              <hr className="style-two"/>
-              <h3>U nas znajdziesz następujące artykuły:</h3>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
+                <hr className="style-two"/>
+                <h3>U nas znajdziesz następujące artykuły:</h3>
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
 
-            <table className="shop-products-table">
-              <thead>
-              <tr>
-                <th> </th>
-                <th>Nazwa</th>
-                <th>Cena</th>
-                <th> </th>
-              </tr>
-              </thead>
-              <tbody>
-              {
-                this.state.shop.ingredients.map(
-                  shopIngredient => ({
-                      ingredient: ingredients.find(
-                        ingredient =>
-                        ingredient.id === shopIngredient.id
-                      ),
-                      price: shopIngredient.ingredientPrice
-                    }
-                  )
-                ).map(
-                  item =>
-                    <tr>
-                      <td>
-                        <Image src={item.ingredient.img}
-                               height="60px"
-                               width="auto"
-                               rounded
-                        />
-                      </td>
-                      <td>
-                        <span className="ingredient-name"> {item.ingredient.name.toUpperCase()}</span>
-                      </td>
-                      <td>
-                       Tylko <span className="red-price">{item.price}</span> zł / 1kg
-                      </td>
-                      <td>
+              <table className="shop-products-table">
+                <thead>
+                <tr>
+                  <th></th>
+                  <th>Nazwa</th>
+                  <th>Cena</th>
+                  <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                  this.state.shop.ingredients.map(
+                      shopIngredient => ({
+                            ingredient: ingredients.find(
+                                ingredient =>
+                                ingredient.id === shopIngredient.id
+                            ),
+                            price: shopIngredient.ingredientPrice
+                          }
+                      )
+                  ).map(
+                      item =>
+                          <tr>
+                            <td>
+                              <Image src={item.ingredient.img}
+                                     height="60px"
+                                     width="auto"
+                                     rounded
+                              />
+                            </td>
+                            <td>
+                              <span className="ingredient-name"> {item.ingredient.name.toUpperCase()}</span>
+                            </td>
+                            <td>
+                              Tylko <span className="red-price">{item.price}</span> zł / 1kg
+                            </td>
+                            <td>
                         <span title="Dodaj do listy zakupów">
                         <GoChecklist className="addToShoppingList"/>
                         </span>
-                      </td>
-                    </tr>
-                )
-              }
-              </tbody>
-            </table>
-          </Modal.Body>
-          <Modal.Footer>
-            <Link onClick={this.close}>
-              <span className="span-button button-close">Zamknij</span>
-            </Link>
-          </Modal.Footer>
-        </Modal>
-      </div>
+                            </td>
+                          </tr>
+                  )
+                }
+                </tbody>
+              </table>
+            </Modal.Body>
+            <Modal.Footer>
+              <Link onClick={this.close}>
+                <span className="span-button button-close">Zamknij</span>
+              </Link>
+            </Modal.Footer>
+          </Modal>
+        </div>
     )
   }
 })
