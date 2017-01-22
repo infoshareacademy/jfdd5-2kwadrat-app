cf1cb5b776a98b3a87696f627d44d82c930a61bc
JakubMelson <kubamelson@gmail.com>
2017-01-22 14:28:21 +0100

    Clean code

--- src/FavouriteReducer/actionCreatos.js
+++ src/FavouriteReducer/actionCreatos.js
@@ -22,7 +22,8 @@ import {
 
 export const fetchShoppingList = (userId, accessToken) => {
   return dispatch => {
-    dispatch({type: FETCH_SHOPPING_LIST__BEGIN})
+    dispatch({type: FETCH_SHOPPING_LIST__BEGIN});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken
     ).then(
@@ -34,23 +35,23 @@ export const fetchShoppingList = (userId, accessToken) => {
           item =>
           item.itemType === 'ingredient'
         ).map(
-          function(item){
+          function (item) {
             const ingredient = {
-              id:item.id,
-              itemId:item.itemId
-            }
+              id: item.id,
+              itemId: item.itemId
+            };
             return ingredient
           }
         )
       })
     )
   }
-}
-
+};
 
 export const addRecipeToFav = (userId, accessToken, ingredientId) => {
   return dispatch => {
-    dispatch({type: ADD_RECIPE_TO_FAV})
+    dispatch({type: ADD_RECIPE_TO_FAV});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken,
       {
@@ -66,11 +67,12 @@ export const addRecipeToFav = (userId, accessToken, ingredientId) => {
       }
     )
   }
-}
+};
 
 export const addToShoppingList = (userId, accessToken, ingredientId) => {
   return dispatch => {
-    dispatch({type: ADD_TO_SHOPPING_LIST})
+    dispatch({type: ADD_TO_SHOPPING_LIST});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken,
       {
@@ -87,15 +89,15 @@ export const addToShoppingList = (userId, accessToken, ingredientId) => {
     ).then(
       function () {
         dispatch(fetchShoppingList(userId, accessToken))
-
       }
     )
   }
-}
+};
 
 export const fetchFavouriteRecipes = (userId, accessToken) => {
   return dispatch => {
-    dispatch({type: FETCH_FAV_RECIPES__BEGIN})
+    dispatch({type: FETCH_FAV_RECIPES__BEGIN});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken
     ).then(
@@ -107,24 +109,24 @@ export const fetchFavouriteRecipes = (userId, accessToken) => {
           item =>
           item.itemType === 'recipe'
         ).map(
-          function(item){
+          function (item) {
             const recipe = {
-              id:item.id,
-              itemId:item.itemId
-            }
+              id: item.id,
+              itemId: item.itemId
+            };
             return recipe
           }
         )
       })
     )
   }
-}
-
+};
 
 
 export const fetchCalendarEvents = (userId, accessToken) => {
   return dispatch => {
-    dispatch({type: FETCH_EVENTS__BEGIN})
+    dispatch({type: FETCH_EVENTS__BEGIN});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken
     ).then(
@@ -136,25 +138,26 @@ export const fetchCalendarEvents = (userId, accessToken) => {
           item =>
           item.itemType === 'event'
         ).map(
-          function(item){
+          function (item) {
             const event = {
               title: item.title,
               end: new Date(item.end),
               start: new Date(item.start),
               id: item.id
-            }
+            };
             return event
           }
         )
       })
     )
   }
-}
+};
 
 
 export const addCalendarEvent = (userId, accessToken, event) => {
   return dispatch => {
-    dispatch({type: ADD_CALENDAR_EVENT})
+    dispatch({type: ADD_CALENDAR_EVENT});
+
     fetch(
       'https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems?access_token=' + accessToken,
       {
@@ -172,102 +175,100 @@ export const addCalendarEvent = (userId, accessToken, event) => {
         })
       }
     ).then(
-     function(){
-       dispatch(fetchCalendarEvents(userId,accessToken))
-     }
+      function () {
+        dispatch(fetchCalendarEvents(userId, accessToken))
+      }
     )
   }
-}
+};
 
 export const removeFromShoppingList = (userId, token, favoriteId) => {
   return (dispatch) => {
     dispatch({
       type: REMOVE_INGREDIENT__BEGIN
-    })
+    });
 
     fetch('https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems/' + favoriteId + '?access_token=' + token, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
       }
+    }
     ).then(
       response => {
         if (response.status === 204) {
           dispatch({
             type: REMOVE_INGREDIENT__SUCCESS
-          })
+          });
           dispatch(fetchShoppingList(userId, token))
-
         }
         else {
           return response.json().then(
-            error => dispatch({type: REMOVE_INGREDIENT__FAIL, error: error})
+              error => dispatch({type: REMOVE_INGREDIENT__FAIL, error: error})
           )
         }
       }
     )
   }
-}
+};
 
 export const removeRecipeFromFav = (userId, token, favoriteId) => {
   return (dispatch) => {
     dispatch({
       type: REMOVE_RECIPE__BEGIN
-    })
+    });
 
     fetch('https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems/' + favoriteId + '?access_token=' + token, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
       }
+    }
     ).then(
       response => {
         if (response.status === 204) {
           dispatch({
             type: REMOVE_RECIPE__SUCCESS
-          })
+          });
           dispatch(fetchFavouriteRecipes(userId, token))
-
         }
         else {
           return response.json().then(
-            error => dispatch({type: REMOVE_RECIPE__FAIL, error: error})
+              error => dispatch({type: REMOVE_RECIPE__FAIL, error: error})
           )
         }
       }
     )
   }
-}
+};
 
 export const removeEvent = (userId, token, favoriteId) => {
   return (dispatch) => {
     dispatch({
       type: REMOVE_EVENT__BEGIN
-    })
+    });
 
     fetch('https://salty-plateau-32425.herokuapp.com/api/users/' + userId + '/favoriteItems/' + favoriteId + '?access_token=' + token, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        }
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
       }
+    }
     ).then(
       response => {
         if (response.status === 204) {
           dispatch({
             type: REMOVE_EVENT__SUCCESS
-          })
+          });
           dispatch(fetchCalendarEvents(userId, token))
 
         }
         else {
           return response.json().then(
-            error => dispatch({type: REMOVE_EVENT__FAIL, error: error})
+              error => dispatch({type: REMOVE_EVENT__FAIL, error: error})
           )
         }
       }
     )
   }
-}
+};
--- src/FavouriteReducer/reducer.js
+++ src/FavouriteReducer/reducer.js
@@ -1,15 +1,13 @@
 import {
   FETCH_FAV_RECIPES__SUCCES,
   FETCH_SHOPPING_LIST__SUCCES,
-  FETCH_EVENTS__SUCCES
-
-}from './actionTypes'
+  FETCH_EVENTS__SUCCES } from './actionTypes'
 
 const initialState = {
-  favouriteRecipes:[],
-  shoppingList:[],
-  calendarEvents:[]
-}
+  favouriteRecipes: [],
+  shoppingList: [],
+  calendarEvents: []
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
@@ -17,17 +15,17 @@ export default (state = initialState, action) => {
       return {
         ...state,
         favouriteRecipes: action.favoriteRecipesId
-      }
+      };
     case FETCH_SHOPPING_LIST__SUCCES:
       return {
         ...state,
         shoppingList: action.shoppingListIds
-      }
+      };
     case FETCH_EVENTS__SUCCES:
       return {
         ...state,
         calendarEvents: action.events
-      }
+      };
     default:
       return state
   }
