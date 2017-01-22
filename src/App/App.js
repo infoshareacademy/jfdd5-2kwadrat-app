ff36a9b9c4c91284d051a3cce51ab8b0aa7d2c22
JakubMelson <kubamelson@gmail.com>
2017-01-22 13:27:55 +0100

    Clean code

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import {Grid, Row} from 'react-bootstrap'
-
 import {NavigationView} from './NavigationView'
 
-// import './App.css'
 
 export default (props) => (
   <div className="App">
--- src/CalendarView/CalendarForm/CalendarForm.js
+++ src/CalendarView/CalendarForm/CalendarForm.js
@@ -1,32 +1,29 @@
 import React from 'react'
-import { FormControl, FormGroup} from 'react-bootstrap'
-
-import '../CalendarView.css'
+import {FormControl, FormGroup} from 'react-bootstrap'
 import recipes from '../../data/recipes'
+import '../CalendarView.css'
+
 
 class CalendarForm extends React.Component {
-
-    render() {
-        return (
-            <div >
-
-
-                <FormGroup>
-                    <FormControl componentClass="select"
-                                 className="zupa"
-                                 id="modalNameInput"
-                                 placeholder="select">
-                        {
-                            recipes.map(
-                                recipe =>
-                                    <option value={recipe.name} key={recipe.name}>{recipe.name}</option>
-                            )
-                        }
-                    </FormControl>
-                </FormGroup>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <FormGroup>
+          <FormControl componentClass="select"
+                       className="zupa"
+                       id="modalNameInput"
+                       placeholder="select">
+            {
+              recipes.map(
+                  recipe =>
+                      <option value={recipe.name} key={recipe.name}>{recipe.name}</option>
+              )
+            }
+          </FormControl>
+        </FormGroup>
+      </div>
+    )
+  }
 
 }
 
